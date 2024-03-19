dd0525e01de96adc9a59ffb45704b086a91d0bdd
Tim Wahrburg <t.wahrburg@gmail.com>
2024-03-19 16:31:44 +0100

    feat: Spider Dance

--- Lunar-Drill/Assets/Main/Scripts/SpiderController.cs
+++ Lunar-Drill/Assets/Main/Scripts/SpiderController.cs
@@ -7,12 +7,13 @@ public class SpiderController : MonoBehaviour
     //--- Exposed Fields ------------------------
 
     [SerializeField][Range(2, 5)] float _innerOrbitRange = 3;
-    [SerializeField][Range(0.1f, 10f)] float _rotationSpeed = 5f;
+    [SerializeField][Range(0.01f, 1f)] float _rotationSpeed = 5f;
 
 
     //--- Private Fields ------------------------
 
     Rigidbody2D _rigidbody;
+    float _orbitRotationT = 0.75f;
 
 
     //--- Unity Methods ------------------------
@@ -24,9 +25,31 @@ public class SpiderController : MonoBehaviour
 
     public void FixedUpdate()
     {
-        
+        AdjustPositionT();
+        SetSpiderPosition();
+        SetSpiderRotation();
     }
 
 
-    //--- Public Methods ------------------------
+    //--- Private Methods ------------------------
+
+    void AdjustPositionT()
+    {
+        _orbitRotationT += _rotationSpeed * Time.deltaTime * Mathf.Sin(Time.time * 2);
+    }
+
+    void SetSpiderPosition()
+    {
+        float angle = _orbitRotationT.Remap(0, 1, 0, 360);
+
+        Vector2 rotatedVector = Quaternion.Euler(0f, 0f, angle) * Vector2.up;
+        Vector2 position = rotatedVector * _innerOrbitRange;
+
+        _rigidbody.MovePosition(position);
+    }
+
+    void SetSpiderRotation()
+    {
+        _rigidbody.MoveRotation(Quaternion.LookRotation(Vector3.forward, transform.position));
+    }
 }
